6948fe29ee62db0ff6bff5b7bdd797c010749034
Christopher V <ChrisRVill@gmail.com>
2025-03-24 23:57:40 -0700

    Testing out methods to create a window

--- src/main.go
+++ src/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"syscall"
 
+	"golang.org/x/sys/windows"
+)
+
+func main() {
+	title := "Hello world"
+	message := "I am a message"
+	t, _ := syscall.UTF16PtrFromString(title)
+	txt, _ := syscall.UTF16PtrFromString(message)
+
+	windows.MessageBox(0, txt, t, 0)
 }
